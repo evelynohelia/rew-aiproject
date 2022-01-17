55754c534b6a373479abd42f18223f9b7dc1119d
Evelyn Mejia <enmejia@espol.edu.ec>
2022-01-17 01:17:42 -0500

    request

--- rewaiproject/clothing/views.py
+++ rewaiproject/clothing/views.py
@@ -1,19 +1,14 @@
-from io import FileIO
-import json
 import cv2
 import os
-import numpy
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from .forms import ImageTestForm
-from PIL import Image
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.decorators.csrf import csrf_exempt
-
 
 def index(request):
     return render(
@@ -31,8 +26,18 @@ def imageTest(request):
         if form.is_valid():
            file = request.FILES['imagen']
            source_folder = os.path.join(settings.STATICFILES_DIRS[0], "img/testImages/")
+           
            file_name = default_storage.save(source_folder+file.name, file)
-           return JsonResponse({'message':'imagen recibida', 'file': file_name})
+           img = cv2.imread(file_name, 0)       
+           img = cv2.resize(img, (256, 256))
+           #model = run_model()
+           
+           #result_camisa = classification.get_mask(img,model, file.name+'test',mode='camisa')
+           #result_pantalon = classification.get_mask(img,model, file.name+'test',mode='pantalon')
+           # metodo para obtener la nueva imagen
+           # file_output_name
+           return JsonResponse({'message':'imagen recibida', 'recibido': file_name, 'result_camisa': 'y',
+           'result_pantalon': 'y'})
         else:
             print(form.errors)    
 
--- rewaiproject/utils/storeClassification.py
+++ rewaiproject/utils/storeClassification.py
@@ -1,14 +1,8 @@
-from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout, Lambda
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, concatenate, Conv2DTranspose, Dropout
 from tensorflow.keras.utils import normalize
-import os
-import glob
-import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib import colors
-from matplotlib import cm
 
 SIZE_X = 256 
 SIZE_Y = 256
@@ -90,6 +84,7 @@ def get_mask(image,model,filename="prediccion.png",mode='camisa'):
     test_img_input=np.expand_dims(image, 0)
     prediction = (model.predict(test_img_input))
     predicted_img=np.argmax(prediction, axis=3)[0,:,:]
+    
     def camisa(imgshow):
         new_image = imgshow
         for i in np.unique(new_image):
@@ -130,119 +125,7 @@ def get_mask(image,model,filename="prediccion.png",mode='camisa'):
         predicted_img = piel(predicted_img)
     else:
         predicted_img = predicted_img
-    fig = plt.imshow(camisa(predicted_img),cmap='gray')
+    #fig = plt.imshow(camisa(predicted_img),cmap='gray')
     plt.axis('off')
     plt.savefig(filename, bbox_inches='tight')
     plt.close()
-
-
-'''
-test_images = []
-for directory_path in glob.glob(os.path.join(os.getcwd(), "clothing\IMAGES")):
-    for img_path in glob.glob(os.path.join(directory_path, "*.png")):
-        img = cv2.imread(img_path, 0)       
-        img = cv2.resize(img, (256, 256))
-        test_images.append(img)
-test_images = np.expand_dims(test_images, axis=3)
-test_images = normalize(test_images, axis=1)
-
-color_list = list(range(59))
-cmap = cm.get_cmap('jet')
-norm = colors.Normalize(vmin=0, vmax=58)
-test_img = np.array(test_images)
-test_img = test_img[19]
-test_img_input=np.expand_dims(test_img, 0)
-prediction = (model.predict(test_img_input))
-predicted_img=np.argmax(prediction, axis=3)[0,:,:]
-print(np.unique(predicted_img))
-
-plt.figure(figsize=(24, 16))
-plt.subplot(231)
-plt.title('Testing Image')
-plt.imshow(test_img[:,:,0], cmap='gray')
-plt.subplot(232)
-plt.title('Prediction on test image')
-imgs = plt.imshow(predicted_img, cmap='jet')
-
-#FFE500
-plt.colorbar(imgs, cmap=cmap, norm=norm, boundaries=np.unique(predicted_img), ticks=np.unique(predicted_img))
-plt.show()
-
-def camisas(imgshow):
-    for i in np.unique(imgshow):
-        if i > 20:
-            imgshow[np.where(imgshow==i)] = 0
-    
-    
-test_img = np.array(test_images)
-test_img = test_img[18]
-test_img_input=np.expand_dims(test_img, 0)
-prediction = (model.predict(test_img_input))
-predicted_img=np.argmax(prediction, axis=3)[0,:,:]
-
-def camisas(imgshow):
-    new_image = imgshow
-    for i in np.unique(new_image):
-        if i > 18:
-            new_image[np.where(new_image==i)] = 0
-        else: 
-            if i > 0:
-                new_image[np.where(new_image==i)] = 10
-    return new_image
-
-
-def pantalon(imgshow):
-    new_image = imgshow
-    for i in np.unique(new_image):
-        if i < 24 or i > 35:
-            new_image[np.where(new_image==i)] = 0
-        else:
-            if i > 0:
-                new_image[np.where(new_image==i)] = 10
-
-    return new_image
-
-def piel(imgshow):
-    new_image = imgshow
-    for i in np.unique(new_image):
-        if i < 35:
-            new_image[np.where(new_image==i)] = 0
-        else:
-            if i > 0:
-                new_image[np.where(new_image==i)] = 10
-    return new_image
-
-
-
-plt.figure(figsize=(22, 14))
-plt.subplot(231)
-plt.title('Testing Image')
-plt.imshow(test_img[:,:,0], cmap='gray')
-plt.subplot(232)
-plt.title('Prediction on test image')
-imgs = plt.imshow(predicted_img, cmap='jet')
-plt.subplot(233)
-plt.title('Shirts/Blouse')
-solo_camisa = camisas(predicted_img)
-imgs = plt.imshow(solo_camisa, cmap='jet')
-
-plt.subplot(234)
-plt.title('Pants/Leggins')
-test_img_input=np.expand_dims(test_img, 0)
-prediction = (model.predict(test_img_input))
-predicted_img=np.argmax(prediction, axis=3)[0,:,:]
-solo_pantalon = pantalon(predicted_img)
-imgs = plt.imshow(solo_pantalon, cmap='jet')
-
-
-plt.subplot(235)
-plt.title('Skin')
-test_img_input=np.expand_dims(test_img, 0)
-prediction = (model.predict(test_img_input))
-predicted_img=np.argmax(prediction, axis=3)[0,:,:]
-solo_piel = piel(predicted_img)
-imgs = plt.imshow(solo_piel, cmap='jet')
-
-plt.savefig('result.png')
-plt.show()
-'''
